0cc4de35d9f0e665e469037f2a18f17fd6d6d322
Constantin <grishanoff.konstantin@yandex.ru>
2022-06-02 11:17:54 +0500

    txt

--- homework/HW3.py
+++ homework/HW3.py
@@ -1,79 +1,13 @@
 
-import math
 
+# 5.  Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа. 
+print()     
+def ex5():
+    data = open("new_text_file_1.txt", "r" )
+    for line in data:
+        print(line)
+    data.close() 
 
-
-# Найти НОК двух чисел
-
-
-def ex1(number1, number2):
-  
-    if (number1==number2): 
-        print ("Числа равны и сами являются НОК")
-        return
-
-    elif (number1>number2):
-        NOD = number2
-        temp1 = number1
-        temp2 = number2
-
-    elif (number1<number2):
-        NOD = number1
-        temp1 = number1
-        temp2 = number2
-    
-    while(temp1%NOD!=0 or temp2%NOD!=0):
-        NOD-=1
-    NOK = number1/NOD*number2
-    print(NOK)
-
-    
-ex1(4, 1)
-
-
-# Вычислить число Пи c заданной точностью d
-# Пример: при d = 0.001,  c= 3.141. 
-
-def ex2(d):
-    number_pi = math.pi
-    new_number_pi = math.floor(number_pi/d)*d
-    print(new_number_pi)
-
-ex2(0.001)
-
-
-# Составить список простых множителей натурального числа N
-
-
-def ex3(N):
-    div = 2
-    spisok = [ ]
-    while(N!=1):
-        if (N%div==0):
-            N/=div
-            spisok.append(div)
-        else: div+=1
-      
-    print(spisok)
-
-ex3(58)
-
-
-
-# Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
-# Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
-
-spisok_elementov_re = [1, 2, 3, 5, 1, 5, 3, 10]
-
-def ex4(spisok_elementov):
-    new_spisok_elementov = []
-    for i in spisok_elementov:
-        if i not in new_spisok_elementov:
-            new_spisok_elementov.append(i)
-
-    print(new_spisok_elementov)
-
-ex4(spisok_elementov_re)
-        
+ex5()
 
 
