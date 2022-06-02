f2fb07168c82dad4287325f106b218d08c2a4069
Constantin <grishanoff.konstantin@yandex.ru>
2022-06-02 20:19:08 +0500

    txt

--- homework/HW3.py
+++ homework/HW3.py
@@ -17,7 +17,7 @@ def ex1(number1, number2):
         temp1 = number1
         temp2 = number2
 
-    elif (number1<number2):
+    else:
         NOD = number1
         temp1 = number1
         temp2 = number2
@@ -52,7 +52,8 @@ def ex3(N):
         if (N%div==0):
             N/=div
             spisok.append(div)
-        else: div+=1
+        else: 
+            div+=1
       
     print(spisok)
 
@@ -74,6 +75,64 @@ def ex4(spisok_elementov):
     print(new_spisok_elementov)
 
 ex4(spisok_elementov_re)
+
+
+
+
+# 5.  Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа. 
+
+
+def ex5():
+    data = open("New_doc.txt", "r+")
+
+    data.close
+
+ex5()
+
+
+# 6 Определите функцию, которая принимает римскую цифру в качестве аргумента и возвращает ее значение 
+# в виде числового десятичного целого числа. Вам не нужно проверять форму римской цифры.
+# Современные римские цифры записываются путем выражения каждой десятичной цифры числа, 
+# которое должно быть закодировано отдельно, начиная с самой левой цифры. Таким образом, 1990 отображается 
+# "MCMXC" (1000 = M, 900 = CM, 90 = XC), а 2008 отображается "MMVIII" (2000 = MM, 8 = VIII). Римская цифра для 1666, 
+# "MDCLXVI", использует каждую букву в порядке убывания.
+# Пример: имя_вашей_функции ('XXI') # должно вернуть 21
+
+rim_num = "MDCLXVI"
+def extra_6(rim_num):
+    rim_dict =  {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    arab_number = 0
+    i = 0
+    while(i<len(rim_num)-1):    # Я так понял внутри цикла Фор итератор и шаг в время выполнения цикла менять нельзя?
+        
+        if rim_dict[rim_num[i]]>=rim_dict[rim_num[i+1]]:
+            arab_number+=rim_dict[rim_num[i]]
+            print(arab_number)
+            i+=1
+           
+        else:
+             arab_number+=rim_dict[rim_num[i+1]]-rim_dict[rim_num[i]]
+             i+=2
+             print(arab_number)
+
+    if (i==len(rim_num)-1): 
+        arab_number+=rim_dict[rim_num[len(rim_num)-1]]
+
+    print(arab_number)
+
+extra_6(rim_num)
+
+
+
+
+
+
+    
+
+
+
+
+
         
 
 
